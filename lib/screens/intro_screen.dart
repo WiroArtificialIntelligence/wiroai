a6f8fb463a7e3db6e0840c15b3d87556e3b02d81
HoyeonS <100548830+HoyeonS@users.noreply.github.com>
2024-06-13 22:04:21 -0700

    add some basic screens

---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Introduction Screen Demo'),
+      ),
+      body: const Center(
+      ),
+    );
+  }
+}
